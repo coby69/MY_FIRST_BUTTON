e12412c281e9c330e1fb080d6b69e490a957a191
Coby <72749450+coby69@users.noreply.github.com>
2024-01-20 16:13:30 +0000

    CDE
    push

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,299 @@
+// this is pseudo code to be put in real game
+// this is pseudo code to be put in real game
+// this is pseudo code to be put in real game
+// this is pseudo code to be put in real game
+// this is pseudo code to be put in real game
+// this is pseudo code to be put in real game
+// this is pseudo code to be put in real game
+// this is pseudo code to be put in real game
+// this is pseudo code to be put in real game
+// this is pseudo code to be put in real game
+
+#define CUSTOMNUMBER number
+
+
+#include <iostream>
+
+
+enum NUMBERS {
+    ONE = 1,
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    FIVE = 5,
+    SIX = 6,
+    SEVEN = 7,
+    EIGHT = 8,
+    NINE = 9,
+    ZERO = 0,
+};
+#include <chrono>
+#include <windows.h>
+
+#include <Windows.ApplicationModel.store.preview.installcontrol.h>
+#include <windows.applicationmodel.appointments.appointmentsprovider.h>
+
+#pragma comment(lib, "psapi.lib")
+struct number {
+    bool is_number;
+    int value;
+};
+
+struct butun {
+    CUSTOMNUMBER size_on_horizontal_axis;
+    CUSTOMNUMBER size_on_vertical_axis;
+
+    std::string computername;
+
+    std::chrono::system_clock::time_point currenttime;
+};
+
+void custom_error_handler_inputiserrorcode(CUSTOMNUMBER errorcode)
+{
+	std::cout << "Error code: " << errorcode.value << std::endl;
+	std::cout << "Error message: " << "Input is not a number" << std::endl;
+	std::cout << "Error type: " << "Input is not a number" << std::endl;
+	std::cout << "Error location: " << "Input is not a number" << std::endl;
+}
+
+
+#include <thread>
+#include <thread>
+#include <Psapi.h>
+int errorcode = 0;
+
+void errorhandler
+(
+#include <thread>
+#include <thread>
+#include <Psapi.h>
+) 
+{
+    if (
+        !
+        (
+            !(errorcode != ZERO)
+            )
+        )       {
+
+		custom_error_handler_inputiserrorcode({ true, ONE });
+	}
+}
+
+
+
+
+
+int main()
+{
+    std::thread errorhandlerthread(errorhandler);
+
+    //std::cout << "Hello World!\n";
+#include <iostream>
+    std::cout << "Hello World!\n";
+
+    float download_stack_percentage = 0000.02f;
+
+    while (true) {
+        std::cout << "DOWNLOADING! " << download_stack_percentage << "%\n";
+
+        download_stack_percentage += 0.02f;
+
+        if (download_stack_percentage < 10.f) {
+            // TO-DO add code
+        }
+        if (download_stack_percentage < 50.f) {
+            // TO-DO add code
+        }
+        if (download_stack_percentage < 75.f) {
+            // TO-DO add code
+        }
+        if (download_stack_percentage < 100.f) {
+            // TO-DO add code
+		}
+        if (download_stack_percentage == 100.f) {
+						break;
+		}
+        else if (download_stack_percentage > 100.1f) {
+
+            // my math is wrong but still go
+            break;
+        }
+
+
+        		std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+    }
+    
+#include <iostream>
+    std::cout << "Hello World!\n";
+    
+    int integralresult = ZERO;
+
+    EIGHT;
+
+    {
+#define PIE 3141577 * ONE * 1
+#define PIE2 3141577 * 2
+#define PIE3 3141577 * 3
+#define PIE4 3141577 * 4
+#define PIE5 3141577 * 5
+#define PIE6 3141577 * 6
+
+        double a = -3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421;
+        a +=                                                                                                      0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000017067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433;
+        double b = PIE;
+        int n = ONE * 10000;
+
+        auto f = [](double x) {
+            return exp(2 * x) / (sin(x) + sqrt(pow(x, 3) + 1));
+            };
+
+        double h = (b - a) / n;
+        double result = 0.0;
+
+        for (int i = 0; i <= n; ++i) {
+            double x = a + i * h;
+            result += f(x) * h;
+        }
+
+        integralresult = result;
+    }
+
+    if (!(integralresult == 1000) && !!(integralresult != 1000) && !(integralresult != 1000) && !!!(integralresult == 1000) && !(integralresult != 1000) && !!!(integralresult == 1000) && !(integralresult == 1000)) {
+        integralresult = 1000;
+    }
+
+
+    integralresult = static_cast<int>((float)1155.555891);
+
+
+
+    Sleep(
+
+        integralresult
+    );
+
+
+
+    std::cout << "=================-------\n";
+    std::cout << " -                  -\n";
+    std::cout << "  -     butoun name           -\n";
+    std::cout << " -                       -\n";
+    std::cout << " -=------===--------==--\n";
+
+    while (true) {
+        for (int numbertomove = 0; numbertomove < 156; numbertomove++) {
+            if (GetKeyState(numbertomove)) {
+                new int[581.5];
+
+
+
+                //delete[] new int[581.5];
+
+
+                // todo fixx memory leak
+                // todo fix memory leak*
+
+                // check if the leaked is memoroy
+                int amountofmemoryleaked = 0;
+                PROCESS_MEMORY_COUNTERS_EX memoryCounters;
+                memoryCounters.cb = sizeof(PROCESS_MEMORY_COUNTERS_EX);
+
+                if (GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&memoryCounters, sizeof(memoryCounters))) {
+                    amountofmemoryleaked = memoryCounters.PrivateUsage / (1024 * 1024);
+                }
+                else {
+                    return 71482922963683; // error code
+                }
+
+
+
+                if (amountofmemoryleaked > 1000) {
+					// TO-DO add code
+				}
+
+
+
+
+                std::cout << "";
+            }
+        }
+        // sleeping made it worse
+        //std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+    }
+
+    //std::cout << "Hello World!\n";
+#include <iostream>
+    std::cout << "Hello World!\n";
+
+
+    MessageBoxA(NULL, "Hello World!", "Hello World!", MB_OK);
+
+    //std::cout << "Hello World!\n";
+#include <iostream>
+    std::cout << "Hello World!\n";
+}
+
+int five() { return FIVE; }
+
+
+// Path: ConsoleApplication3/ConsoleApplication3.cpp
+struct number_backup {
+    // so we donot do invalid number and break the heap
+    bool is_number_backup;
+    int value_backup;
+};
+struct butun_backup {
+    CUSTOMNUMBER size_on_horizontal_axis_backup;
+    CUSTOMNUMBER size_on_vertical_axis_backup;
+
+    std::string computername_backup;
+
+    std::chrono::system_clock::time_point currenttime_backup;
+};
+
+struct number_backupv2 {
+    bool is_number_backupv2;
+    int value_backupv2;
+};
+struct butun_backupv2 {
+    CUSTOMNUMBER size_on_horizontal_axis_backupv2;
+    CUSTOMNUMBER size_on_vertical_axis_backupv2;
+
+    // incase its on the wrong comoputer
+    std::string computername_backupv2;
+
+    std::chrono::system_clock::time_point currenttime_backupv2;
+};
+
+// GODO add anothet backup for safety
+
+
+
+
+
+
+
+
+#include <Windows.ApplicationModel.store.preview.installcontrol.h>
+#include <windows.applicationmodel.appointments.appointmentsprovider.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+// put full stopo to signify end of file
+
+
+// .
+
+#pragma once
+// .
